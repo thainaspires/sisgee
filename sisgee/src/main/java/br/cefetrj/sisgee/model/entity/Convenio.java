6682384b82013bc384eb1597cd4209d117faaf24
Anderson Luiz <labrocha1991@gmail.com>
2017-11-18 19:41:19 -0200

    Add entity
    Classes Convenio e empresa adicionadas

---
+++ sisgee/src/main/java/br/cefetrj/sisgee/model/entity/Convenio.java
@@ -0,0 +1,83 @@
+package br.cefetrj.sisgee.model.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+/**
+ * Entidade Convenio
+ * @author Anderson Luiz
+ * @since 1.0
+ *
+ */
+@Entity
+public class Convenio {
+	@Id
+	@GeneratedValue
+	private Long idConvenio;
+	private String numeroConvenio;
+	
+	@ManyToOne(fetch=FetchType.EAGER)
+	private Empresa idEmpresa;
+
+	
+	public Long getIdConvenio() {
+		return idConvenio;
+	}
+
+	public void setIdConvenio(Long idConvenio) {
+		this.idConvenio = idConvenio;
+	}
+
+	public String getNumeroConvenio() {
+		return numeroConvenio;
+	}
+
+	public void setNumeroConvenio(String numeroConvenio) {
+		this.numeroConvenio = numeroConvenio;
+	}
+
+	public Empresa getIdEmpresa() {
+		return idEmpresa;
+	}
+
+	public void setIdEmpresa(Empresa idEmpresa) {
+		this.idEmpresa = idEmpresa;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idConvenio == null) ? 0 : idConvenio.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Convenio other = (Convenio) obj;
+		if (idConvenio == null) {
+			if (other.idConvenio != null)
+				return false;
+		} else if (!idConvenio.equals(other.idConvenio))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+}
